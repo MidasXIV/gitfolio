63f0c81700e9576af2445f006a7ea384354e4565
MidasXIV <amanshaikh.arc@gmail.com>
2020-07-18 17:29:15 +0400

    Update About page with Tailwind CSS classes

--- pages/about.jsx
+++ pages/about.jsx
@@ -17,62 +17,24 @@ export default class About extends Component {
     const { user } = this.props;
 
     return (
-      <Layout title="About" class="container">
-        <main>
-          <h1 className="title">About Page of the Gitfolio</h1>
-          <img src={user.avatar_url} alt="User Github logo" height="200px" />
-          <p className="description">
-            {user.login} - {user.bio}
-          </p>
-        </main>
-        <style jsx>{`
-          .container {
-            min-height: 100vh;
-            padding: 0 0.5rem;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-      
-          main {
-            padding: 5rem 2rem;
-            flex: 1;
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-            align-items: center;
-          }
-      
-          .title a {
-            color: #0070f3;
-            text-decoration: none;
-          }
-      
-          .title a:hover,
-          .title a:focus,
-          .title a:active {
-            text-decoration: underline;
-          }
-      
-          .title {
-            margin: 0;
-            line-height: 1.15;
-            font-size: 4rem;
-          }
-      
-          .title,
-          .description {
-            text-align: center;
-          }
-      
-          .description {
-            line-height: 1.5;
-            font-size: 1.5rem;
-          }
-        `}</style>
+      <Layout title="About">
+        <div className="px-4 py-4">
+          <div className="sm:max-w-xs md:max-w-xs sm:w-1/2 max-w-sm rounded overflow-hidden shadow-lg bg-gray-300">
+            <img className="rounded-lg" src={user.avatar_url} alt="User Github logo" />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">{user.login}</div>
+              <p className="text-gray-700 text-base">
+                {user.bio}
+              </p>
+            </div>
+            <div className="px-6 py-4">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+            </div>
+          </div>
+        </div>
       </Layout>
-
     )
   }
 }
