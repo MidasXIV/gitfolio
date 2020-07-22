4b4b76c8f1c826257d2eafe0eccab15e09f23eef
MidasXIV <amanshaikh.arc@gmail.com>
2020-07-23 01:51:59 +0400

    Responsive header and Footer update

--- layouts/default/index.jsx
+++ layouts/default/index.jsx
@@ -15,9 +15,9 @@ const Layout = ({ children, title }) => (
       <title>{title}</title>
     </Head>
 
-    <header>
+    <header className="min-w-full flex flex-col md:flex-row justify-between">
       <div>
-
+        <h6 className="px-4 py-4 uppercase font-mono font-semibold text-xs border-b md:border-none">Gitfolio</h6>
       </div>
       <div className="flex justify-end">
         <Link href="/"><a className="px-4 py-4 uppercase font-mono font-semibold text-xs border-l">Home</a></Link>
@@ -29,20 +29,19 @@ const Layout = ({ children, title }) => (
 
     {children}
 
-    <footer className="min-w-full flex flex-col md:flex-row">
+    <footer className="bottom-0 min-w-full flex flex-col md:flex-row uppercase font-mono font-semibold text-xs">
       <div className="py-4 border-b w-full md:w-3/4 md:border-r">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-          className="uppercase font-mono font-semibold text-xs"
         >
           Powered by{' '}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </div>
       <div className="md:w-1/4 py-4  w-full bg-gray-700">
-        
+        <a className="text-gray-400">Gitfolio Podcast</a>
       </div>
     </footer>
   </div >
