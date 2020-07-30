5767fee3d6de15d924fdd50ab48b06657ede1898
MidasXIV <amanshaikh.arc@gmail.com>
2020-07-31 02:19:33 +0400

    Update blog content styling

--- layouts/blog/index.jsx
+++ layouts/blog/index.jsx
@@ -14,9 +14,15 @@ export default class BlogLayout extends Component {
     const { slug, title, author, content } = blog;
     return (
       <div>
-        <article className="markdown-body px-4 py-4 mx-auto xs:w-full sm:w-1/2">
+        <article className="blog-border px-4 py-4 mx-auto xs:w-full sm:w-1/2">
           <ReactMarkdown source={content} className={styles.markdown} />
         </article>
+        <style jsx>{`
+          .blog-border {
+            border-left: 1px solid #eaeaea;
+            border-right: 1px solid #eaeaea;
+          }
+        `}</style>
       </div>
     );
   }
